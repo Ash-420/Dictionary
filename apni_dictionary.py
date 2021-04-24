043653bee8ffce8725a3ce51a2217a529ae0775f
Ash <58181368+Ash-420@users.noreply.github.com>
2021-04-24 16:16:45 +0530

    Add files via upload

---
+++ apni_dictionary.py
@@ -0,0 +1,9 @@
+d1 = {"Strap":"A gun or firearm, usually a pistol.",
+    "Ducking":"Leaving early or hiding, sometimes in order to avoid a certain person or a group of people.",
+    "Gat":"A firearm, usually a pistol or open-bolt submachine-gun, used for busting caps into someone/something's punk ass.",
+    "Makaveli":"Tha don of all dons, the boss playaz of playaz, tupac!!",
+    "CPT":"Compton, California. Many people will sometimes refer to Compton as The CPT. It is an abbreviation for Compton and has been mentioned by rappers in various songs."}
+
+print("Enter the Word for its meaning")
+Word = input()
+print(d1[Word])
